f140084ba4459f6fc6c6eab60e1afce298e8ce85
Aditya Gupta <aditya.gupta6@BLRETV-L0TYMD6M.local>
2023-05-18 23:04:52 +0530

    added ProffessorOperationService

--- src/com/flipkart/services/ProfessorOperationService.java
+++ src/com/flipkart/services/ProfessorOperationService.java
@@ -1,4 +1,39 @@
 package com.flipkart.services;
 
 public class ProfessorOperationService {
+
+    public void addGrades(){
+
+        System.out.println("add grades for students");
+    }
+
+    public void viewEnrolledStudents(){
+
+        System.out.println("Enrolled studnets are viewed");
+    }
+
+    public void createProfessor(){
+        System.out.println("create professor here");
+    }
+
+    public boolean deleteProfessor(int id){
+        System.out.println("delete professor with " + id + "id");
+
+        return true;
+    }
+
+
+
+    public boolean updateDetails() {
+        System.out.println("Impl of updateDetails of UserBusiness");
+
+        return true;
+    }
+
+    public boolean updatePassword(String newPassword) {
+        System.out.println("Impl of updatePassword of UserBusiness");
+
+        return true;
+    }
+
 }
