887d141c9d94b611906cd4773605661da17cb5fe
MintuJupally <mintu.jupally@gmail.com>
2023-05-19 00:04:38 +0530

    Updated services

--- src/com/flipkart/services/LoginOperationService.java
+++ src/com/flipkart/services/LoginOperationService.java
@@ -1,18 +1,17 @@
 package com.flipkart.services;
 
-import com.flipkart.bean.Student;
 import com.flipkart.bean.User;
 
 public class LoginOperationService extends User {
-    public User login(String role, String username, String password){
-        System.out.println("Verify user credentials");
+	public User login(String role, String username, String password) {
+		System.out.println("Logging user in");
 
-        return null;
-    }
+		return null;
+	}
 
-    public boolean changePassword(String role, String userId, String oldPass, String newPass){
-        System.out.println("update user's password here");
+	public boolean changePassword(String role, String userId, String oldPass, String newPass) {
+		System.out.println("Changing password of " + role);
 
-        return true;
-    }
+		return true;
+	}
 }
