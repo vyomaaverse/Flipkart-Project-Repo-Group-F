5b049ed739473a83d990cccb6543b74fc5f73e74
avantika <avantika.mudumbai@gmail.com>
2023-05-18 23:57:52 +0530

    Application client

--- src/com/flipkart/client/StudentManagementClient.java
+++ src/com/flipkart/client/StudentManagementClient.java
@@ -5,24 +5,24 @@ import com.flipkart.bean.Student;
 import java.util.Scanner;
 
 public class StudentManagementClient {
-    public static void main(String[] args){
-
-
-        Scanner role = new Scanner(System.in);
-        System.out.println("Enter roles: \n 1. Student\n 2. Professor \n");
-        int role1 = role.nextInt();
-
-        if(role1==1){
-            Student student = new Student();
-        }
-        else if(role1 == 2){
-
-        }
-        else{
-            System.out.println("Enter correct option");
-        }
-
-
-
-    }
+//    public static void main(String[] args){
+//
+//
+//        Scanner role = new Scanner(System.in);
+//        System.out.println("Enter roles: \n 1. Student\n 2. Professor \n");
+//        int role1 = role.nextInt();
+//
+//        if(role1==1){
+//            Student student = new Student();
+//        }
+//        else if(role1 == 2){
+//
+//        }
+//        else{
+//            System.out.println("Enter correct option");
+//        }
+//
+//
+//
+//    }
 }
