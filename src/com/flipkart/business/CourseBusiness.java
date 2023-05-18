ca73e18e78c8c849a5c99f6571678003084e76b9
Avantika Mudumbai <avantika.mudumbai@BLRETV-C02FM34W.local>
2023-05-18 15:50:19 +0530

    some business classes

--- src/com/flipkart/business/CourseBusiness.java
+++ src/com/flipkart/business/CourseBusiness.java
@@ -2,5 +2,15 @@ package com.flipkart.business;
 
 import com.flipkart.bean.Course;
 
-public class CourseBusiness extends Course {
+public class CourseBusiness extends Course{
+    public void createCourse(){
+        System.out.println("course creation logic");
+    }
+
+    public boolean deleteCourse(String code) {
+        System.out.println("implement delete course");
+        return true;
+    }
+
+
 }
