5792611126456e6be12901477780019421820aab
avantika <avantika.mudumbai@gmail.com>
2023-05-19 12:10:34 +0530

    merge conflict

--- src/com/flipkart/client/CRSstudentMenu.java
+++ src/com/flipkart/client/CRSstudentMenu.java
@@ -1,43 +1,58 @@
 package com.flipkart.client;
 
-import com.flipkart.services.ProfessorOperationService;
 import com.flipkart.services.StudentOperationService;
 
 import java.util.Scanner;
 
 public class CRSStudentMenu {
-    static void showCRSStudentMenu(){
 
-        StudentOperationService sos= new StudentOperationService();
+	private static void displayMenu(){
+		System.out.println("\n------------------------------");
+		System.out.println("Student Menu");
+		System.out.println(" 0. Logout");
+		System.out.println(" 1. View Grade Card");
+		System.out.println(" 2. Register");
+		System.out.println(" 3. Change Password");
+		System.out.print("Your Choice: ");
+	}
+	public static void showCRSStudentMenu() {
+
+		StudentOperationService sos = new StudentOperationService();
+
+		System.out.println("Welcome Student.");
+		Scanner scanner = new Scanner(System.in);
 
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("\n==========================================================================\n");
-        System.out.println("Student Menu");
-        System.out.println(" 1. View Grade Card");
-        System.out.println(" 2. Register");
-        System.out.println(" 3. Change Password");
-        System.out.println("\n==========================================================================\n");
+		displayMenu();
 
 
-        System.out.println("Enter Option : ");
+		int optionChosen = scanner.nextInt();
+		scanner.nextLine();
 
-        int optionChosen = scanner.nextInt();
-        scanner.nextLine();
+		while(optionChosen != 0){
+			switch (optionChosen) {
+				case 1:
+					sos.viewGradeCard(3);
+					break;
 
-        switch (optionChosen) {
-            case 1:
-                sos.viewGradeCard(3);
-                break;
+				case 2:
+					sos.register("name", "branch", 2023);
+					break;
 
-            case 2:
-                sos.register(2, "abc");
-                break;
+				case 3:
+//			sos.changePassword("new");
+					break;
+				default:
+					System.out.println("Invalid operation");
+			}
 
-            case 3:
-                sos.changePassword("new");
-                break;
-        }
-}
+			displayMenu();
+			optionChosen = scanner.nextInt();
+			scanner.nextLine();
+		}
+
+		System.out.println("Thank you student. Logging out");
+
+//		scanner.close();
+	}
 }
